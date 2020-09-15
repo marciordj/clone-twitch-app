9dd2ebe9fe2a08914f55294a39a7c3342c4f3178
Marcio Rodrigues <luiz.rodrigues@eduzz.com>
2020-09-14 22:28:30 -0300

    feat: Add routes

---
+++ src/routes.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+const { Navigator, Screen } = createBottomTabNavigator();
+
+const Routes = () => {
+  <NavigationContainer>
+    <Navigator>
+      <Screen name='Following' component={View} />
+      <Screen name='Discover' component={View} />
+      <Screen name='Browse' component={View} />
+      <Screen name='Esports' component={View} />
+    </Navigator>
+  </NavigationContainer>
+}
+
+
+
+export default Routes;
